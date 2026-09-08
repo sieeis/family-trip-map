d15ae7697d837b2189be8a773a60033efb22b363
sieeis <hankil.generation@gmail.com>
2026-09-08 16:23:21 +0900

    config: set NCP Maps client ID

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 // public/js/config.js
 // 네이버 클라우드 플랫폼(https://console.ncloud.com)에서 발급받은 Maps API 클라이언트 ID를 입력하세요.
 window.APP_CONFIG = {
-  naverMapClientId: 'YOUR_NCP_CLIENT_ID_HERE'
+  naverMapClientId: 'j4yghood90'
 };
